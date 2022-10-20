d3f9e0258060324eeea72443beb313cd4704b62e
Innovin <102300150+InnovinAC@users.noreply.github.com>
2022-10-20 14:07:05 +0100

    Update login.php

--- Week 4 PHP Tasks > Loops and Functions/userAuth/php/login.php
+++ Week 4 PHP Tasks > Loops and Functions/userAuth/php/login.php
@@ -1,10 +1,11 @@
 <?php
 if(isset($_POST['submit'])){
-    $username = //finish this line
-    $password = //finish this
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
 loginUser($email, $password);
-
+    
+    
 }
 
 function loginUser($email, $password){
@@ -12,6 +13,18 @@ function loginUser($email, $password){
         Finish this function to check if username and password 
     from file match that which is passed from the form
     */
+    
+    $file = fopen('../storage/users.csv', 'r');
+    $user_details = fgetcsv($file);
+    if($username == $user_details[1] && $username == $user_details[2]) {
+        $_SESSION['username'] = $username;
+        header('location: ./dashboard.php');
+    }
+    
+    else {
+        header('location: ./login.php');
+        
+    }
 }
 
 echo "HANDLE THIS PAGE";
