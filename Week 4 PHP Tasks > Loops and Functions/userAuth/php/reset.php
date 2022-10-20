12202342c45ce95b5b6e5e7056caa661d9163be8
Innovin <102300150+InnovinAC@users.noreply.github.com>
2022-10-20 14:40:33 +0100

    Update reset.php

--- Week 4 PHP Tasks > Loops and Functions/userAuth/php/reset.php
+++ Week 4 PHP Tasks > Loops and Functions/userAuth/php/reset.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['submit'])){
-    $email = //complete this;
-    $newpassword = //complete this;
+    $email = $_POST['email'];
+    $newpassword = $_POST['password'];
 
     resetPassword($email, $password);
 }
@@ -9,6 +9,21 @@ if(isset($_POST['submit'])){
 function resetPassword($email, $password){
     //open file and check if the username exist inside
     //if it does, replace the password
+    
+    $file = fopen('../storage/users.csv', 'w');
+    $user_details = fgetcsv($file);
+    $user_email = $user_details[1];
+    $user_name = $user_details[0];
+    if($email == $user_email) {
+        $details = array($user_name, $email, $password);
+        fputcsv($file, $details);   
+    }
+    
+    else {
+        echo "User does not exist";
+    }
+    
+    
 }
 echo "HANDLE THIS PAGE";
 
