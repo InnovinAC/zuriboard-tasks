2c91424be3223cdfb66e8c3de27a0b2a13381fe8
Innovin <102300150+InnovinAC@users.noreply.github.com>
2022-10-20 14:47:24 +0100

    Update logout.php

--- Week 4 PHP Tasks > Loops and Functions/userAuth/php/logout.php
+++ Week 4 PHP Tasks > Loops and Functions/userAuth/php/logout.php
@@ -1,10 +1,27 @@
 <?php
+session_start();
+logout();
 function logout(){
     /*
 Check if the existing user has a session
 if it does
 destroy the session and redirect to login page
 */
+    // if user is logged in
+    if($_SESSION['username']) {
+        session_unset();
+        session_destroy();
+        
+        // redirect
+        header('location: login.php');
+    }
+    
+    // otherwise just redirect to login page
+    else {
+        
+        header('location: login.php')
+            
+        }
 }
 
 echo "HANDLE THIS PAGE";
